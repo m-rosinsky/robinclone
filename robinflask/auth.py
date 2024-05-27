ae726617f0e7fff70a90a34823dad1f26c72cecc
m-rosinsky <michael.rosinsky@uconn.edu>
2024-05-27 17:20:36 +0000

    Added login_required wrapper

--- robinflask/auth.py
+++ robinflask/auth.py
@@ -79,4 +79,14 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('auth.login'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
